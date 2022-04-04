cc233d2f6544b712d992a5570059c92eecb154a3
Davistao <614707404@qq.com>
2022-04-04 19:01:10 +0800

    创建了construct文件

---
+++ include/davis_construct.h
@@ -0,0 +1,43 @@
+#include <new>
+namespace DAVIS
+{
+    template <class T1, class T2>
+    inline void construct(T1 *p, const T2 &value)
+    {
+        new (p) T1(value);
+    }
+
+    template <class T>
+    inline void destroy(T *pointer)
+    {
+        pointer->~T();
+    }
+
+    template <class ForwardIterator>
+    inline void destroy(ForwardIterator first, ForwardIterator last)
+    {
+        __destroy(first, last, value_type(first));
+    }
+
+    template <class ForwardIterator, class T>
+    inline void __destroy(ForwardIterator first, ForwardIterator last, T *)
+    {
+        typedef typename __type_traits<T>::has_trivial_destructor trivial_destructor;
+        __destroy_aux(first, last, trivial_destructor());
+    }
+
+    template <class ForwardIterator>
+    inline void __destroy_aux(ForwardIterator first, ForwardIterator last, __false_type)
+    {
+        for (; first != last; first++)
+        {
+            destroy(&*first);
+        }
+    }
+
+    template <class ForwardIterator>
+    inline void __destroy_aux(ForwardIterator first, ForwardIterator last, __true_type) {}
+
+    inline void destroy(char *, char *) {}
+    inline void destroy(wchar_t *, wchar_t *) {}
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
-#include<iostream>
-#include<vector>
-#include"davis_alloc.h"
+#include <iostream>
+#include <vector>
+#include "davis_allocate.h"
 using namespace std;
-int main(){
-    cout<<"Hello MiniSTL"<<endl;
-    int ia[5]={0,1,2,3,4};
+int main()
+{
+    cout << "Hello MiniSTL" << endl;
+    int ia[5] = {0, 1, 2, 3, 4};
     unsigned int i;
-    vector<int,DAVIS::allocator<int>> iv(ia,ia+5);
-    for(i = 0;i<iv.size();i++){
-        cout<<iv[i]<<' ';
+    vector<int, DAVIS::allocator<int>> iv(ia, ia + 5);
+    for (i = 0; i < iv.size(); i++)
+    {
+        cout << iv[i] << ' ';
     }
-    cout<<endl;
+    cout << endl;
     return 0;
 }
