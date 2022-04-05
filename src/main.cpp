6779d528f1057514baed7d9bf058878e66696505
614707404 <614707404@qq.com>
2022-04-05 22:36:43 +0800

    添加type_traits

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 #include <vector>
 #include "davis_allocate.h"
+#include "davis_vector.h"
 using namespace std;
 int main()
 {
     cout << "Hello MiniSTL" << endl;
     int ia[5] = {0, 1, 2, 3, 4};
-    unsigned int i;
-    vector<int, DAVIS::allocator<int>> iv(ia, ia + 5);
-    for (i = 0; i < iv.size(); i++)
+    DAVIS::vector<int> test(5, 1);
+    for (int i = 0; i < test.size(); i++)
     {
-        cout << iv[i] << ' ';
+        cout << test[i] << ' ';
     }
+    // vector<int, DAVIS::allocator<int>> iv(ia, ia + 5);
+    // for (vector<int, DAVIS::allocator<int>>::iterator it = iv.begin(); it != iv.end(); it++)
+    // {
+    //     cout << *it << ' ';
+    // }
     cout << endl;
     return 0;
 }
